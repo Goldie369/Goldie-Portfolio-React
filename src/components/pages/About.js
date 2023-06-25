1d6c86009f5885526f0ece835a7891c887c74adb
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-06-25 16:02:41 -0500

    adding discription on the about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -21,6 +21,8 @@ export default function About() {
           <h3 className="about-me-header" style={{ color: '#161615', textAlign:"center", fontWeight: "bold", marginBottom: "5vh" }}>About Me</h3>
           <p style={{ color: '#161615', paddingLeft: "6vw", paddingRight: "6vw", textAlign:"center" }}>
             My name is Goldie Rizova!!!
+            I develop cool websites.
+As a Web Developer I aim to build unique, well-designed, user-friendly websites. You will find a few sample projects - please view my github account to see all of my work.
           </p>
         </div>
        
